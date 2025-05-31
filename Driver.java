03477401559c02aca841f8eb97c2ed620c07dc3e
Welpyfish <williamzhou2015@gmail.com>
2025-05-30 21:22:04 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,34 @@
+import java.io.File;
+
 public class Driver {
-public static void main(String [] args) {
+public static void main(String [] args) throws Exception {
 Polynomial p = new Polynomial();
 System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
+double [] c1 = {6,5};
+int[] e1 = {0, 3};
+Polynomial p1 = new Polynomial(c1, e1);
+double [] c2 = {-2,-9};
+int[] e2 = {1, 4};
+Polynomial p2 = new Polynomial(c2, e2);
 Polynomial s = p1.add(p2);
 System.out.println("s(0.1) = " + s.evaluate(0.1));
 if(s.hasRoot(1))
 System.out.println("1 is a root of s");
 else
 System.out.println("1 is not a root of s");
+
+
+Polynomial s2 = p1.multiply(p2);
+System.out.println("s2(0.1) = " + s2.evaluate(0.1));
+if(s2.hasRoot(1))
+        System.out.println("1 is a root of s2");
+else
+    System.out.println("1 is not a root of s2");
+
+s2.saveToFile("poly.txt");
+Polynomial f = new Polynomial(new File("poly.txt"));
+    System.out.println("f(0.1) = " + f.evaluate(0.1));
+
 }
+
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,29 +1,127 @@
+import java.io.*;
 
 class Polynomial{
 
 	double[] coeff;
+	int[] exp;
 
-	Polynomial(){coeff = new double[]{0};}
+	Polynomial(){
+		coeff = new double[]{};
+		exp = new int[]{};
+	}
 
-	Polynomial(double[] coeff){this.coeff = coeff;}
+	Polynomial(double[] coeff, int[] exp){
+		this.coeff = coeff;
+		this.exp = exp;
+	}
+
+	Polynomial(File file) throws Exception {
+		this();
+		BufferedReader input = new BufferedReader(new FileReader(file));
+		String line = input.readLine();
+		input.close();
+
+		String[] p = line.split("(?=[+-])");
+
+		coeff = new double[p.length];
+		exp = new int[p.length];
+		for(int i=0; i<p.length; i++){
+			String[] t = p[i].split("x");
+
+			coeff[i]=Double.parseDouble(t[0]);
+			if(t.length==1){
+				exp[i]=0;
+			}else{
+				exp[i]=Integer.parseInt(t[1]);
+			}
+		}
+
+	}
 
 	Polynomial add(Polynomial p){
-		double[] result = new double[Math.max(coeff.length, p.coeff.length)];
-		for(int i=0; i<result.length; i++){
-			if(i<coeff.length){result[i]+=coeff[i];}
-			if(i<p.coeff.length){result[i]+=p.coeff[i];}
+		int l=coeff.length+ p.coeff.length;
+
+		double[] newcoeff = new double[l];
+		int[] newexp = new int[l];
+
+		int i=0;
+		int j=0;
+		int a=0;
+
+		while(i<exp.length || j<p.exp.length){
+
+			if(j==p.exp.length || (i<exp.length && exp[i]<p.exp[j])){
+				newexp[a]=exp[i];
+				newcoeff[a]=coeff[i];
+				i++;
+				a++;
+			}else if(i == exp.length || exp[i] > p.exp[j]){
+				newexp[a]=p.exp[j];
+				newcoeff[a]=p.coeff[j];
+				j++;
+				a++;
+			}else if(exp[i] == p.exp[j]){
+				if(coeff[i]+p.coeff[j]!=0){
+					newexp[a]=exp[i];
+					newcoeff[a]=coeff[i]+p.coeff[j];
+					i++;
+					j++;
+					a++;
+				}
+			}else{
+				break;
+			}
 		}
-		return new Polynomial(result);
+
+		double[] newcoeff2 = new double[a];
+		int[] newexp2 = new int[a];
+		for(int b=0; b<a; b++){
+			newcoeff2[b]=newcoeff[b];
+			newexp2[b]=newexp[b];
+		}
+
+		return new Polynomial(newcoeff2, newexp2);
 	}
 
 	double evaluate(double x){
 		double result=0;
 		for(int i=0; i<coeff.length; i++){
-			result+=coeff[i]*Math.pow(x, i);
+			result+=coeff[i]*Math.pow(x, exp[i]);
 		}
 		return result;
 
 	}
 
 	boolean hasRoot(double x){ return 0==evaluate(x);}
+
+	Polynomial multiply(Polynomial p){
+
+		Polynomial result = new Polynomial();
+
+		for(int j=0; j<p.exp.length; j++){
+			for(int i=0; i<exp.length; i++){
+				result = result.add(new Polynomial(
+						new double[]{coeff[i] * p.coeff[j]},
+						new int[]{exp[i]+p.exp[j]}
+						)
+				);
+
+			}
+		}
+		return result;
+	}
+
+	void saveToFile(String filename) throws Exception {
+		PrintStream output = new PrintStream(filename);
+		for(int i=0; i<exp.length; i++){
+			if(coeff[i]>0){
+				output.print("+");
+			}
+			output.print(coeff[i]);
+			if(exp[i]!=0){
+				output.print("x"+exp[i]);
+			}
+		}
+		output.close();
+	}
 }
